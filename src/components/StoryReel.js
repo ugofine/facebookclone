2b5edc31a5e8ae2da04bf55609611b7c5ced50e8
Debbie <62076688+ugofine@users.noreply.github.com>
2021-09-10 12:23:53 +0100

    changes to the styling

--- src/components/StoryReel.js
+++ src/components/StoryReel.js
@@ -2,7 +2,6 @@ import Story from './Story'
 import pic from '../images/pic.jpg'
 import chi from '../images/chi.jpg'
 import avat1 from '../images/avat1.png'
-import avat from '../images/avat.jpg'
 import womanlawyer from '../images/womanlawyer.jpg'
 import './StoryReel.css'
 
@@ -12,7 +11,7 @@ const StoryReel = () => {
             <Story  profilePic={pic} name="Debbie Ogu" alt="debbie" bgimage={chi}/>
             <Story profilePic={pic} name="Debbie Ogu" alt="debbie" bgimage={avat1}/>
             <Story profilePic={pic} name="Debbie Ogu" alt="debbie" bgimage={womanlawyer} />
-            <Story  profilePic={pic} name="Debbie Ogu" alt="debbie" bgimage={avat}/>
+            {/* <Story  profilePic={pic} name="Debbie Ogu" alt="debbie" bgimage={avat}/> */}
             {/* <Story  profilePic={pic} name="Debbie Ogu" alt="debbie" bgimage={chi}/>
             <Story profilePic={pic} name="Debbie Ogu" alt="debbie" bgimage={womanlawyer} /> */}
         </div>
